170750abc89501619977eb32df2d8147f22408c3
phukpongphanot <phukpongphanot2543@gmail.com>
2020-03-24 23:26:15 +0700

    initial commit

--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse # คือมีการส่งข้อความตอบกลับ ต้องfrom http เข้ามาทำงาน
 from django.contrib.auth.models import User,auth # เวลาที่เราจะบันทึกขึ้นมูลที่เกี่ยวข้องกับ models user ต้อง from และ import อันนี้
 from django.contrib import messages  #คือการนำข้อความไปแสดงที่ถ้าเว็บเลย ในที่นี้คือเป็นข้อความ errror
+
+from random import randrange
+import pymysql
+import json
 # Create your views here.
+def connect_db():
+    con = pymysql.connect(host='localhost',
+                          user='root',
+                          password='a',
+                          db='pj_camera')
+    return con
+
 def hello(request):
    #return HttpResponse("<h2>Hello non</h2>") #ตัวอักษรหน้าเว็บ
    #return render(request,'index.html') # การดึกข้อมูลจาก html ออกไป
@@ -35,7 +46,7 @@ def Table(request):
     return render(request,'Table.html')
 
 def register(request):
-   return render(request,'register.html')
+    return render(request,'register.html')
 
 def addregister(request):
     StudentID = request.POST['StudentID']
@@ -62,6 +73,15 @@ def addregister(request):
             email = Email,
             password = Password
             )
+            db = connect_db()
+            try:
+                cursor = db.cursor()
+                cursor.execute(insert_sql('info', data))
+                db.commit()
+            except:
+                print('error')
+            db.close()
+
             user.save()
             return render(request,'result.html')
     else:
@@ -103,3 +123,67 @@ def RanNumBack(request):
  
 def key_Num(request):
     return render(request,'key_Num.html')
+
+
+def insert_sql(table_name, value_json):
+    print('difeijofpajnvgirgjnj')
+    sql = ""
+    if table_name == 'info':
+        sql = "INSERT INTO `info` (`ID`, `PASSWORD`, `NAME`, `STATUS`, `CAMERA_ID`, `TIME`) \
+         VALUES ('%d', '%d', '%s', '%d', '%d', '%d')" % \
+              (value_json["id"], value_json["password"], value_json["name"], value_json["status"], value_json["camera_id"],value_json["time"])
+    elif table_name == 'user_info':
+        sql = "INSERT INTO `user_info` (`ID`, `LAST_ACTION`, `HISTORY`, `REPORT`, `PERSONAL`) \
+         VALUES ('%d', '%s', '%s', '%s', '%s')" %\
+              (value_json["id"],value_json['last_action'],value_json["history"],value_json['report'],value_json["personal"])
+    return sql
+
+
+
+with open("data.json", 'r') as f:
+    data = json.load(f)
+    data = data["user_id"]["1"]
+
+# {
+#   "user_id": {
+#     "1": {
+#       "id": 123456,
+#       "password": 123256,
+#       "name": 'Ryujin Jakra ภูเขาไฟ',
+#       "status": 2,
+#       "camera_id": 102030,
+#       "time": 10.00,
+#       "last_action": 2,
+#       "history":  2,
+#       "report": 2,
+#       "personal": {
+#         "age": 2,
+#         "sex": "male"
+#       }
+#     },
+#     "2": {
+#       "id": 123456,
+#       "password": 123256,
+#       "name": 'Ryujin Jakra ภูเขาไฟ',
+#       "status": 2,
+#       "camera_id": 102030,
+#       "time": 10.00,
+#       "last_action": 2,
+#       "history":  2,
+#       "report": 2,
+#       "personal": {
+#         "age": 2,
+#         "sex": "male"
+#       }
+#     }
+#   }
+# }
+
+
+
+
+
+# with open("data.json", "r") as read_file:
+#     data = json.load(read_file)
+#     data = data["user_id"]["1"]
+
